8fe017c93530afafe3797bbbc415c6af4d8afe99
hablijack <christoph.habel@posteo.de>
2023-03-19 18:13:13 +0100

    1.6.0: history charts fixed

--- pages/history.vue
+++ pages/history.vue
@@ -2,36 +2,52 @@
   <v-container fluid>
     <v-row>
       <v-col cols="12">
-        <v-card variant="outlined" class="pa-8 bgwhite">
-          <canvas id="airtemperature"></canvas>
-        </v-card>
+        <v-sheet elevation="4" rounded color="white" class="pa-8">
+          <h3 class="text-center">Lufttemperaturen</h3>
+          <div class="mb-4">
+            <v-btn type="outlined" id="day">Tag</v-btn>
+            <v-btn type="outlined" id="week" :active="true">Woche</v-btn>
+            <v-btn type="outlined" id="month">Monat</v-btn>
+          </div>
+          <canvas id="air-temperatures"></canvas>
+        </v-sheet>
+      </v-col>
+    </v-row>
+
+    <v-row>
+      <v-col cols="12">
+        <v-sheet elevation="4" rounded color="white" class="pa-8">
+          <h3 class="text-center">Luftfeuchtigkeit</h3>
+          <div class="mb-4">
+            <v-btn type="outlined" id="day">Tag</v-btn>
+            <v-btn type="outlined" id="week" :active="true">Woche</v-btn>
+            <v-btn type="outlined" id="month">Monat</v-btn>
+          </div>
+          <canvas id="air-humidity"></canvas>
+        </v-sheet>
       </v-col>
     </v-row>
   </v-container>
 </template>
-
-<style>
-.bgwhite {
-  background-color: white;
-}
-</style>
 
 <script setup>
 import { Chart, registerables } from "chart.js";
 import 'chartjs-adapter-moment';
 
 const config = useRuntimeConfig()
-const timerange = ref("week");
+const air_temperatures_timerange = ref("week");
+const air_humidity_timerange = ref("week");
 
-const { data: datasets } = await useLazyFetch(config.apiBaseUrl + `/backend/history/air/temperatures?range=${timerange}`);
+const { data: air_temperatures } = await useFetch(config.apiBaseUrl + `/backend/history/air/temperatures?range=${air_temperatures_timerange}`);
+const { data: air_humidity } = await useFetch(config.apiBaseUrl + `/backend/history/air/humidity?range=${air_humidity_timerange}`);
 
 onMounted(() => {
   if (Chart) {
     Chart.register(...registerables);
 
-    new Chart("airtemperature", {
+    new Chart("air-temperatures", {
       type: 'line',
-      data: { datasets: datasets.value },
+      data: { datasets: air_temperatures.value },
       options: {
         responsive: true,
         elements: {
@@ -43,7 +59,7 @@ onMounted(() => {
           x: {
             type: 'time',
             time: {
-              unit: 'month'
+              unit: 'day'
             }
           }
         },
@@ -54,6 +70,28 @@ onMounted(() => {
         }
       },
     });
+
+    new Chart("air-humidity", {
+      type: 'bar',
+      data: { datasets: air_humidity.value },
+      options: {
+        responsive: true,
+        scales: {
+          x: {
+            type: 'time',
+            time: {
+              unit: 'day'
+            }
+          }
+        },
+        plugins: {
+          legend: {
+            position: 'bottom',
+          }
+        }
+      },
+    });
+
   }
 });
 </script>
