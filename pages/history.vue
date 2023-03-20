f9fdcf135d8210cc85f490cab46c5b5270c481ec
hablijack <christoph.habel@posteo.de>
2023-03-20 14:41:31 +0100

    1.7.0: history charts fixed

--- pages/history.vue
+++ pages/history.vue
@@ -27,6 +27,48 @@
         </v-sheet>
       </v-col>
     </v-row>
+
+    <v-row>
+      <v-col cols="12">
+        <v-sheet elevation="4" rounded color="white" class="pa-8">
+          <h3 class="text-center">WiFI Signalstärke</h3>
+          <div class="mb-4">
+            <v-btn type="outlined" id="day">Tag</v-btn>
+            <v-btn type="outlined" id="week" :active="true">Woche</v-btn>
+            <v-btn type="outlined" id="month">Monat</v-btn>
+          </div>
+          <canvas id="wifi"></canvas>
+        </v-sheet>
+      </v-col>
+    </v-row>
+
+    <v-row>
+      <v-col cols="12">
+        <v-sheet elevation="4" rounded color="white" class="pa-8">
+          <h3 class="text-center">CO2 Gehalt</h3>
+          <div class="mb-4">
+            <v-btn type="outlined" id="day">Tag</v-btn>
+            <v-btn type="outlined" id="week" :active="true">Woche</v-btn>
+            <v-btn type="outlined" id="month">Monat</v-btn>
+          </div>
+          <canvas id="co2"></canvas>
+        </v-sheet>
+      </v-col>
+    </v-row>
+
+    <v-row>
+      <v-col cols="12">
+        <v-sheet elevation="4" rounded color="white" class="pa-8">
+          <h3 class="text-center">Sonneneinstrahlung (Helligkeit)</h3>
+          <div class="mb-4">
+            <v-btn type="outlined" id="day">Tag</v-btn>
+            <v-btn type="outlined" id="week" :active="true">Woche</v-btn>
+            <v-btn type="outlined" id="month">Monat</v-btn>
+          </div>
+          <canvas id="brightness"></canvas>
+        </v-sheet>
+      </v-col>
+    </v-row>
   </v-container>
 </template>
 
@@ -37,17 +79,23 @@ import 'chartjs-adapter-moment';
 const config = useRuntimeConfig()
 const air_temperatures_timerange = ref("week");
 const air_humidity_timerange = ref("week");
+const wifi_timerange = ref("week");
+const co2_timerange = ref("week");
+const brightness_timerange = ref("week");
+const battery_timerange = ref("week");
+
 
 const { data: air_temperatures } = await useFetch(config.apiBaseUrl + `/backend/history/air/temperatures?range=${air_temperatures_timerange}`);
 const { data: air_humidity } = await useFetch(config.apiBaseUrl + `/backend/history/air/humidity?range=${air_humidity_timerange}`);
+const { data: wifi } = await useFetch(config.apiBaseUrl + `/backend/history/wifi?range=${wifi_timerange}`);
+const { data: co2 } = await useFetch(config.apiBaseUrl + `/backend/history/co2?range=${co2_timerange}`);
+const { data: brightness } = await useFetch(config.apiBaseUrl + `/backend/history/brightness?range=${brightness_timerange}`);
+const { data: battery } = await useFetch(config.apiBaseUrl + `/backend/history/battery?range=${battery_timerange}`);
 
-onMounted(() => {
-  if (Chart) {
-    Chart.register(...registerables);
-
-    new Chart("air-temperatures", {
+const createChart = (identifier, datasets, scale, unit) => {
+  new Chart(identifier, {
       type: 'line',
-      data: { datasets: air_temperatures.value },
+      data: { datasets },
       options: {
         responsive: true,
         elements: {
@@ -61,26 +109,12 @@ onMounted(() => {
             time: {
               unit: 'day'
             }
-          }
-        },
-        plugins: {
-          legend: {
-            position: 'bottom',
-          }
-        }
-      },
-    });
-
-    new Chart("air-humidity", {
-      type: 'bar',
-      data: { datasets: air_humidity.value },
-      options: {
-        responsive: true,
-        scales: {
-          x: {
-            type: 'time',
-            time: {
-              unit: 'day'
+          },
+          y: {
+            ticks: {
+                callback: function(value, index, ticks) {
+                    return value + unit;
+                }
             }
           }
         },
@@ -91,7 +125,17 @@ onMounted(() => {
         }
       },
     });
+}
 
+onMounted(() => {
+  if (Chart) {
+    Chart.register(...registerables);
+    createChart('air-temperatures', air_temperatures.value, '°C', 'day' );
+    createChart('air-humidity', air_humidity.value, '%', 'day' );
+    createChart('wifi', wifi.value, '%', 'day' );
+    createChart('co2', co2.value, 'ppm', 'day' );
+    createChart('brightness', brightness.value, 'cp', 'day' );
+    createChart('battery', battery.value, '%', 'day' );
   }
 });
 </script>
